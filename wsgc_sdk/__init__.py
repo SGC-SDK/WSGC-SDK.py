29af423d3d0944d498a5549a16646e95f4711299
CyberRex <hspwinx86@gmail.com>
2021-09-11 23:53:30 +0900

    Change version

--- wsgc_sdk/__init__.py
+++ wsgc_sdk/__init__.py
@@ -10,7 +10,7 @@ __title__ = 'wsgc-sdk'
 __author__ = 'CyberRex'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2021 CyberRex'
-__version__ = '1.0'
+__version__ = '1.0a'
 
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 
